a8fc806e1a5f30414820dfd7d72d49d1b4591175
Polygonull <polygonull@gmail.com>
2020-05-05 09:45:37 +0300

    Change polygonull background

--- polygonull_website/scripts/main.js
+++ polygonull_website/scripts/main.js
@@ -1,7 +1,5 @@
 var startAll = function () {
-    particlesJS.load('null-particles3', 'config/particles3.json');
-    particlesJS.load('null-particles4', 'config/particles4.json');
-    particlesJS.load('null-particles6', 'config/particles6.json');
+    particlesJS.load('null-particles', 'config/particlesjs.json');
 };
 
 if (document.readyState === "complete" || (document.readyState !== "loading" && !document.documentElement.doScroll)) {
